997b2f99d9e11d3fca2421eb9da749cf0057f874
amitairferreira <amitairlima@gmail.com>
2022-07-25 21:35:06 -0300

    index

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -2,12 +2,8 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  res.status(200).send({
-    title: 'Projeto final Reprograma: Voluntários do Campo',
-    version: '1.0.0',
-    mensagem:
-      ' Essa é a primeira versão deste projeto, uma API focada em cadastro de produtores da agricultura familiar e profissionais voluntários da agropecuária.'
-  })
+  res.send(
+    'Projeto: Voluntários do campo. Cadastra, atualiza, pesquisa por cidade e por Id produtores e profissionais'
+  )
 })
-
 module.exports = routes
