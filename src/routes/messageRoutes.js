05e0b06d45bc5339c721f1835a847e1fc88f78fc
amitairferreira <amitairlima@gmail.com>
2022-07-28 13:31:18 -0300

    autenticando rotas de menssagens

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,14 +1,14 @@
 const controller = require('../controller/messageController')
-// const { checkAuth } = require('../middlewares/auth')
+const { checkAuth } = require('../middlewares/auth')
 const express = require('express')
 
 const router = express.Router()
 
-router.post('/send', controller.sendMessage)
-router.get('/sendTo', controller.sendToId)
-router.get('/received', controller.receivedToId)
-router.patch('/deleteMessageFarmer/:id', controller.deleteMessageFarmer)
-router.patch('/deleteMessageProfessional/:id', controller.deleteMessageProfessional)
+router.post('/send', checkAuth, controller.sendMessage)
+router.get('/sendTo', checkAuth, controller.sendToId)
+router.get('/received', checkAuth, controller.receivedToId)
+router.patch('/deleteMessageFarmer/:id', checkAuth, controller.deleteMessageFarmer)
+router.patch('/deleteMessageProfessional/:id', checkAuth, controller.deleteMessageProfessional)
 
 
 module.exports = router;
